860107fe4e19b8ecf140684e10814a84a6b6136c
Lucas Ribeiro D'Azevedo <128042824+luconnes@users.noreply.github.com>
2025-09-22 23:52:17 +0000

    super commit botao add carrinho

--- site/app/components/Card.js
+++ site/app/components/Card.js
@@ -1,6 +1,5 @@
 import styles from '../styles/Card.module.css';
 
-
 export default function Card({ imagem, titulo, conjunto, preco }) {
   return (
     <div className={styles.card}>
@@ -8,6 +7,12 @@ export default function Card({ imagem, titulo, conjunto, preco }) {
       <div className={styles['card-title']}>{titulo}</div>
       <div className={styles['card-set']}>{conjunto}</div>
       <div className={styles['card-price']}>{preco}</div>
+      <button
+        className={styles['card-button']}
+        onClick={() => alert(`Adicionado ${titulo} ao carrinho!`)}
+      >
+        Adicionar ao carrinho
+      </button>
     </div>
   );
 }
