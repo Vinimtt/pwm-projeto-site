9d9300e3535c731913023c2cdb02516da43dc786
Joao Victor <joao.vr.fernandes@gmail.com>
2025-09-22 17:30:10 -0300

    css aplicados a navBar

--- site/app/components/navBar.js
+++ site/app/components/navBar.js
@@ -1,21 +1,25 @@
 import Link from "next/link"
 import React from "react";
+import styles from "../styles/NavBar.module.css";
 import { FaShoppingCart, FaHome  } from "react-icons/fa";
 import { AiOutlineStock } from "react-icons/ai";
 
 
 const NavBar = () => {
     return (
-        <div>
-            <ul>
-                <li>
+        <div className={styles.navbar}>
+            <ul className={styles.navbarList}>
+                <li className={styles.navbarItem}>
                     <Link href={"/"}> Home <FaHome /> </Link>
                 </li>
-                <li>
+                <li className={styles.navbarItem}>
                     <Link href={"/"}> Mercado <AiOutlineStock /> </Link>
                 </li>
-                <li>
-                    <Link href={"/"}> <FaShoppingCart /> </Link>
+                <li className={styles.navbarItem}>
+                    <Link href={"/"} className={styles.navbarLink}> <FaShoppingCart /> </Link>
+                </li>
+                <li className={styles.navbarItem}>
+                    <Link href={"/Login"}> Login </Link>
                 </li>
             </ul>
         </div>
